5be8f54d34bd1d3a5e947866852c60d256a2f5c5
Anthony Laliberte <anthony.laliberte.1@ulaval.ca>
2024-12-09 19:15:41 -0500

    cleaned up MemorySegment.cpp

--- MemorySegment.cpp
+++ MemorySegment.cpp
@@ -7,35 +7,34 @@
 #include "MemorySegment.h"
 
 //Constructeur
-MemorySegment::MemorySegment(size_t startAddress, size_t size, AccessType accessType)
-: startAddress(startAddress), size(size), accessType(accessType), memory(size, 0) {
-
-}
+MemorySegment::MemorySegment(size_t startAddress, size_t size, AccessType accessType) :
+    startAddress(startAddress), size(size), accessType(accessType), memory(size, 0) {}
 
 //Methode pour tester le type d'access
-bool  MemorySegment::canAccess(size_t address, AccessType requestType) const{
-    return address >= startAddress && address < startAddress + size &&
-           (requestType == READ_ONLY || accessType == READ_WRITE);
+bool MemorySegment::canAccess(size_t address, AccessType requestType) const{
+    bool addressIsInMemorySegment = address >= startAddress && address < startAddress + size;
+    bool requestIsAllowed = (requestType == READ_ONLY || accessType == READ_WRITE);
+    return addressIsInMemorySegment && requestIsAllowed;
 }
 
 //Methode pour lire un char dans une adresse
-char  MemorySegment::read(size_t address) const{
+char MemorySegment::read(size_t address) const{
     if (!canAccess(address, READ_ONLY)) throw std::runtime_error("Access violation");
-    return memory[address - startAddress];
+    return memory.at(address - startAddress);
 }
 //Methode pour ecrire la valeur un char dans une adresse
-void  MemorySegment::write(size_t address, char value){
+void MemorySegment::write(size_t address, char value){
     if (!canAccess(address, READ_WRITE)) throw std::runtime_error("Access violation");
-    memory[address - startAddress] = value;
+    memory.at(address - startAddress) = value;
 }
 
 //Methode getter avoir le debut d'adresse du segment
-size_t  MemorySegment::getStartAddress() const{
+size_t MemorySegment::getStartAddress() const{
     return startAddress;
 }
 
 //Methode pour avoir l'adresse de fin du segment
-size_t  MemorySegment::getEndAddress() const{
+size_t MemorySegment::getEndAddress() const{
     return startAddress + size;
 }
 
